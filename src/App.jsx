6a0e587b7dbeb8aadb175fd1d846c2f230cc1889
ferozalipanhwar <ferozalipanhwar36@gmail.com>
2025-11-02 17:38:45 +0500

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
 
 import './App.css'
-
+import AboutUs from './components/About/AboutUs'
+import BlogSection from './components/Blog/BlogSection'
+import ContactSection from './components/Contact/ContactSection'
+import CoursesSection from './components/Courses/CoursesSection'
+import EducationSection from './components/education/EducationSection'
+import FeaturesSection from './components/Feature/FeatureSection'
+import Footer from './components/Footer/Footer'
+import Home from './components/home/Home'
+import Navbar from './components/navbar/Navbar'
+import TeachersSection from './components/Teachers/TeachersSection'
+import TestimonialsSection from './components/Testimonials/TestimonialsSection'
 function App() {
 
   return (
     <>
-      <h1>Alif Akh  Academy</h1>
+      <Navbar />
+      <Home />
+      <EducationSection/>
+      <FeaturesSection/>
+      <CoursesSection/>
+      <TeachersSection/>
+      <TestimonialsSection/>
+      <BlogSection/>
+      <ContactSection/>
+      <AboutUs/>
+      <Footer/>
     </>
   )
 }
